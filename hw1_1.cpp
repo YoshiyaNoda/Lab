0f8a5a0685ecc65603e6b815e4f76f7c8f1273cc
Yoshiya Noda <ny1425.0106@gmail.com>
2021-02-27 23:46:59 +0900

    いろいろ

--- hw1_1.cpp
+++ hw1_1.cpp
@@ -6,8 +6,8 @@
 constexpr double m = 0.7;
 constexpr double k = 0.2;
 constexpr double g = 9.81;
-constexpr double dt = 0.1;
-constexpr double N = 200; // ループ回数
+constexpr double dt = 0.0001;
+constexpr double N = 20000; // ループ回数
 
 // 初期条件
 constexpr double t0 = 0;
