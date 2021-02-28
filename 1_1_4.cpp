e0182b75c8b3fe5fa262cabe62698d8373d1be07
Yoshiya Noda <ny1425.0106@gmail.com>
2021-02-28 21:02:07 +0900

    いい感じにできた

--- 1_1_4.cpp
+++ 1_1_4.cpp
@@ -11,37 +11,98 @@ constexpr double g = 9.81;
 constexpr double t0 = 0;
 constexpr double v0 = 0;
 
+std::ofstream eOutputfile("youkaihou_error_114.txt");
+std::ofstream inkaihouErrorOutputfile("inkaihou_error_114.txt");
+std::ofstream runge2ErrorOutputfile("runge2_error_114.txt");
+std::ofstream runge4ErrorOutputfile("runge4_error_114.txt");
+
 double youkaihouNextApproximateV(double v, double dt) {
-    return (g - k/m * v) * dt + v;
+    return (g - k / m * v) * dt + v;
+}
+double inkaihouNextApproximateV(double v, double dt) {
+    return (v + g * dt) / (1 + k / m * dt);
+}
+double runge2NextApproximateV(double v, double dt) {
+    return v + dt * (g - k / m * (v + dt / 2 * (g - k / m * v)));
+}
+double runge4NextApproximateV(double v, double dt) {
+    double result;
+    {
+        double v1 = v + dt / 2 * (g - k / m * v);
+        double v2 = v + dt / 2 * (g - k / m * v1);
+        double v3 = v + dt * (g - k / m * v2);
+        result = v + dt / 6 * ((g - k / m * v) + 2 * (g - k / m * v1) + 2 * (g - k / m * v2) + (g - k / m * v3));
+    }
+    return result;
 }
 double nextApproximateT(double t, double dt) {
     return t + dt;
 }
 double culcTheoreticalValue(double t) {
-    return v0 + m * g / k *(1 - std::exp(-k / m * t));
+    return v0 + m * g / k * (1 - std::exp(-k / m * t));
 }
 
-void youkaihou(double dt) {
+void youkaihou(double dt, int N) {
     double t = t0;
     double v = v0;
-    double t = t0;
-    std::ofstream eOutputfile("youkaihou_error_114.txt");
-    // t = 3 s となるようにループの回数を決める
-    for(int i = 0; t < 30001; i++) {
+    for(int i = 0; i < N + 1; i++) {
         // t = 3 s
-        if(i == 30000) {
-            eOutputfile << t << "\t" << std::abs(culcTheoreticalValue(t) - v) << "\n";
+        if(i == N) {
+            eOutputfile << dt << "\t" << std::abs(culcTheoreticalValue(t) - v) << "\n";
         }
-        v = youkaihouNextApproximateV(v, dt);
         t = nextApproximateT(t, dt);
+        v = youkaihouNextApproximateV(v, dt);
     }
-    eOutputfile.close();
+}
+void inkaihou(double dt, int N) {
+    double t = t0;
+    double v = v0;
+    for(int i = 0; i < N + 1; i++) {
+        // t = 3 s
+        if(i == N) {
+            inkaihouErrorOutputfile << dt << "\t" << std::abs(culcTheoreticalValue(t) - v) << "\n";
+        }
+        t = nextApproximateT(t, dt);
+        v = inkaihouNextApproximateV(v, dt);
+    }
+}
+void runge2(double dt, int N) {
+    double t = t0;
+    double v = v0;
+    for(int i = 0; i < N + 1; i++) {
+        // t = 3 s
+        if(i == N) {
+            runge2ErrorOutputfile << dt << "\t" << std::abs(culcTheoreticalValue(t) - v) << "\n";
+        }
+        t = nextApproximateT(t, dt);
+        v = runge2NextApproximateV(v, dt);
+    }
+}
+void runge4(double dt, int N) {
+    double t = t0;
+    double v = v0;
+    for(int i = 0; i < N + 1; i++) {
+        // t = 3 s
+        if(i == N) {
+            runge4ErrorOutputfile << dt << "\t" << std::abs(culcTheoreticalValue(t) - v) << "\n";
+        }
+        t = nextApproximateT(t, dt);
+        v = runge4NextApproximateV(v, dt);
+    }
 }
 
 int main() {
-    double dt = 0.0001;
-    for(i = 0; i < 100; i++) {
-        youkaihou(dt)
-        dt += 0.01;
+    for(int i = 500; i < 10001; i++) {
+        // 切り捨てられないように
+        double div = i;
+        double dt = 3 / div;
+        youkaihou(dt, i);
+        inkaihou(dt, i);
+        runge2(dt, i);
+        runge4(dt, i);
     }
+    eOutputfile.close();
+    inkaihouErrorOutputfile.close();
+    runge2ErrorOutputfile.close();
+    runge4ErrorOutputfile.close();
 }
