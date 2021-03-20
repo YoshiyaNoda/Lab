4d972e4a1d83b7710d787416f2bab2087b1f7915
Yoshiya Noda <ny1425.0106@gmail.com>
2021-03-20 19:47:54 +0900

    速度の初期化方法を変更

--- no3/free.cpp
+++ no3/free.cpp
@@ -17,21 +17,19 @@ std::uniform_real_distribution<double> distr(rand_min, rand_max);
 std::vector<double> vRamdomConfig() {
     return {distr(eng), distr(eng), distr(eng)};
 }
-std::vector<std::vector<std::vector<double>>> culcHexData(std::vector<double> head_loc) {
-    const std::vector<std::vector<double>> vs = {vRamdomConfig(), vRamdomConfig(), vRamdomConfig(), vRamdomConfig()};
+std::vector<std::vector<double>> culcHexLoc(std::vector<double> head_loc) {
     const std::vector<double> r1 = {head_loc[0] + a, head_loc[1] + a, head_loc[2]};
     const std::vector<double> r2 = {head_loc[0], head_loc[1] + a, head_loc[2] + a};
     const std::vector<double> r3 = {head_loc[0] + a, head_loc[1], head_loc[2] + a};
-    const std::vector<std::vector<std::vector<double>>> data = {{head_loc, vs[0]}, {r1, vs[1]}, {r2, vs[2]}, {r3, vs[3]}};
-    return data;
+    return {head_loc, r1, r2, r3};
 }
 void testPlot(const std::vector<std::vector<std::vector<double>>> ini) {
-    std::ofstream initial_loc("test/no3-initial-loc.txt");
+    std::ofstream initial_loc_stream("test/no3-initial-loc.txt");
     for(int i = 0; i < N; i++) {
         auto r = ini[i][0];
-        initial_loc << r[0] << "\t" << r[1] << "\t" << r[2] << "\n";
+        initial_loc_stream << r[0] << "\t" << r[1] << "\t" << r[2] << "\n";
     }
-    initial_loc.close();
+    initial_loc_stream.close();
 }
 void gnuplot() {
     FILE *gp;
@@ -58,18 +56,55 @@ void gnuplot() {
 	fprintf(gp, "exit\n");
 	pclose(gp);
 }
+std::vector<std::vector<double>> normalize(std::vector<std::vector<double>> vs) {
+    double sum_x = 0.0;
+    double sum_y = 0.0;
+    double sum_z = 0.0;
+    for(int i = 0; i < N; i++) {
+        sum_x += vs[i][0];
+        sum_y += vs[i][1];
+        sum_z += vs[i][2];
+    }
+    const double x_mean = sum_x / N;
+    const double y_mean = sum_y / N;
+    const double z_mean = sum_z / N;
+    std::vector<std::vector<double>> normalized = vs;
+    for(int i = 0; i < N; i++) {
+        normalized[i][0] -= x_mean;
+        normalized[i][1] -= y_mean;
+        normalized[i][2] -= z_mean;
+    }
+    return normalized;
+}
+std::vector<std::vector<double>> randomizedV() {
+    std::vector<std::vector<double>> vs(N, std::vector<double>(3));
+    for(int i = 0; i < N; i++) {
+        vs[i] = vRamdomConfig();
+    }
+    return normalize(vs);
+}
+std::vector<std::vector<std::vector<double>>> zip(std::vector<std::vector<double>> rs, std::vector<std::vector<double>> vs) {
+    std::vector<std::vector<std::vector<double>>> res(N, std::vector<std::vector<double>>(2));
+    if(rs.size() == vs.size() && rs.size() == N) {
+        for(int i = 0; i < N; i++) {
+            res[i][0] = rs[i];
+            res[i][1] = vs[i];
+        }
+    }
+    return res;
+}
 // ここで初期位置、初期速度、初期温度の設定
 std::vector<std::vector<std::vector<double>>> config() {
-    std::vector<std::vector<std::vector<double>>> molecular_data(N, std::vector<std::vector<double>>(2));
+    std::vector<std::vector<double>> initial_loc(N, std::vector<double>(3));
     // どの分子か->座標か速度か->その値(3次元)
     std::vector<double> head_loc = {0, 0, 0};
     // ここでは正四面体の頭の座標を更新していいく 2*aずつヅラせばいい
     for(int i = 0; i < N / 4; i++) {
-        const std::vector<std::vector<std::vector<double>>> hex_data = culcHexData(head_loc);
-        molecular_data[i * 4] = hex_data[0];
-        molecular_data[i * 4 + 1] = hex_data[1];
-        molecular_data[i * 4 + 2] = hex_data[2];
-        molecular_data[i * 4 + 3] = hex_data[3];
+        const std::vector<std::vector<double>> hex_loc = culcHexLoc(head_loc);
+        initial_loc[i * 4] = hex_loc[0];
+        initial_loc[i * 4 + 1] = hex_loc[1];
+        initial_loc[i * 4 + 2] = hex_loc[2];
+        initial_loc[i * 4 + 3] = hex_loc[3];
         if(i % 4 == 3) {
             head_loc[0] = 0;
             head_loc[1] += 2 * a;
@@ -81,8 +116,8 @@ std::vector<std::vector<std::vector<double>>> config() {
             head_loc[2] += 2 * a;
         }
     }
-
-    return molecular_data;
+    std::vector<std::vector<double>> initial_v = randomizedV();
+    return zip(initial_loc, initial_v);
 }
 
 int main() {
