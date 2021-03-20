18725ab00a7cbc75dee609cebcf945c07b6d5258
Yoshiya Noda <ny1425.0106@gmail.com>
2021-03-20 19:15:29 +0900

    プロットしてみた

--- no3/free.cpp
+++ no3/free.cpp
@@ -25,6 +25,39 @@ std::vector<std::vector<std::vector<double>>> culcHexData(std::vector<double> he
     const std::vector<std::vector<std::vector<double>>> data = {{head_loc, vs[0]}, {r1, vs[1]}, {r2, vs[2]}, {r3, vs[3]}};
     return data;
 }
+void testPlot(const std::vector<std::vector<std::vector<double>>> ini) {
+    std::ofstream initial_loc("test/no3-initial-loc.txt");
+    for(int i = 0; i < N; i++) {
+        auto r = ini[i][0];
+        initial_loc << r[0] << "\t" << r[1] << "\t" << r[2] << "\n";
+    }
+    initial_loc.close();
+}
+void gnuplot() {
+    FILE *gp;
+	if ((gp = popen(GNUPLOT_PATH, "w")) == NULL) {
+		fprintf(stderr, "ファイルが見つかりません %s.", GNUPLOT_PATH);
+		exit(EXIT_FAILURE);
+	}
+    fprintf(gp, "set xlabel \"x\"\n");
+    fprintf(gp, "set ylabel \"y\"\n");
+    fprintf(gp, "set zlabel \"z\"\n");
+    // fprintf(gp, "set lmargin 10\n");
+    // fprintf(gp, "set bmargin 4\n");
+    // fprintf(gp, "set xlabel font 'Arial,13'\n");
+    // fprintf(gp, "set ylabel font 'Arial,13'\n");
+    fprintf(gp, "set pointsize 0.5\n");
+    // fprintf(gp, "set yrange [0:0.6]\n");
+    // fileの読み込みが最後じゃないとうまくいかなかったのなんでだろう。比較的重めのIOなので、それが間接的な原因になってる気がする。コルーチン化したら直る気がするけどめんどいのでこれでいいや。
+    fprintf(gp, "sp \"./test/no3-initial-loc.txt\" title \"location\" pt 7\n");
+    fflush(gp);
+	
+    std::string dummy;
+    std::cout << "Enter to continue..." << std::endl;
+    std::getline(std::cin, dummy);
+	fprintf(gp, "exit\n");
+	pclose(gp);
+}
 // ここで初期位置、初期速度、初期温度の設定
 std::vector<std::vector<std::vector<double>>> config() {
     std::vector<std::vector<std::vector<double>>> molecular_data(N, std::vector<std::vector<double>>(2));
@@ -55,4 +88,6 @@ std::vector<std::vector<std::vector<double>>> config() {
 int main() {
     const std::vector<std::vector<std::vector<double>>> ini = config();
     std::cout << ini.size() << std::endl;
+    testPlot(ini);
+    gnuplot();
 }
